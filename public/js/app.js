3140baa2755a274c05f54d7094bc014466aa5c01
Kratos <desmond8500@gmail.com>
2020-09-04 14:25:51 +0000

    route redirection update

--- public/js/app.js
+++ public/js/app.js
@@ -54015,8 +54015,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\kratos\Desktop\Projets\Dash3\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\kratos\Desktop\Projets\Dash3\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\Tyto\Desktop\Projets\Dash\Dash3\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\Tyto\Desktop\Projets\Dash\Dash3\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })

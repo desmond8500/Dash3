657241174c45ad245d4c99031be49fbdd069c3d8
kratos <desmond8500@gmail.com>
2024-07-11 09:57:27 +0000

    fix

--- resources/views/livewire/erp/invoice-page.blade.php
+++ resources/views/livewire/erp/invoice-page.blade.php
@@ -1,7 +1,7 @@
 <div>
     @component('components.layouts.page-header', ['title'=>'Devis', 'breadcrumbs'=>$breadcrumbs])
         <div class="btn-list">
-            {{-- @livewire('form.task-add', ['devis_id' => $devis->id]) --}}
+            @livewire('form.task-add', ['devis_id' => $devis->id])
             {{-- @livewire('form.article-add') --}}
             <button class="btn btn-primary" wire:click="$dispatch('open-addSection')">
                 <i class="ti ti-plus"></i> Section

0dd6ef978f4d3587775502661472d1191e5eca4c
kratos <desmond8500@gmail.com>
2024-07-14 21:51:54 +0000

    article fix

--- resources/views/livewire/stock/articles-page.blade.php
+++ resources/views/livewire/stock/articles-page.blade.php
@@ -7,15 +7,12 @@
         </div>
     @endcomponent
 
-    <div class="row g-2">
+    {{-- <div class="row g-2">
         <div class="col-md-2">
             <div>
                 <div class="input-group">
                     <input type="text" class="form-control" wire:model.live="search" placeholder="Rechercher" wire:click="articleSearch">
-                    {{-- <input type="text" class="form-control" wire:model.defer="search" placeholder="Rechercher" wire:keydown.enter='getArticles'> --}}
-                    {{-- <button class="btn btn-primary btn-icon" wire:click="getArticles">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-search" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"> <path stroke="none" d="M0 0h24v24H0z" fill="none"></path> <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0"></path> <path d="M21 21l-6 -6"></path> </svg>
-                    </button> --}}
+
                     @if ($search)
                         <button class="btn btn-secondary btn-icon" wire:click="resetSearch">
                             <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-x" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"> <path stroke="none" d="M0 0h24v24H0z" fill="none"></path> <path d="M18 6l-12 12"></path> <path d="M6 6l12 12"></path> </svg>
@@ -39,10 +36,10 @@
 
             </div>
         </div>
-    </div>
+    </div> --}}
 
 
-    @component('components.modal', ["id"=>'editArticle', 'title'=>'Editer un article'])
+    {{-- @component('components.modal', ["id"=>'editArticle', 'title'=>'Editer un article'])
         <form class="row" wire:submit="update">
             @include('_form.article_form')
             <div class="modal-footer">
@@ -55,5 +52,5 @@
         </form>
         <script> window.addEventListener('open-editArticle', event => { $('#editArticle').modal('show'); }) </script>
         <script> window.addEventListener('close-editArticle', event => { $('#editArticle').modal('hide'); }) </script>
-    @endcomponent
+    @endcomponent --}}
 </div>

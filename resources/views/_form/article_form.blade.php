b165e08dfe8737e678f1bc7c28d8e21024e94906
kratos <desmond8500@gmail.com>
2024-07-16 21:39:35 +0000

    fix

--- resources/views/_form/article_form.blade.php
+++ resources/views/_form/article_form.blade.php
@@ -8,14 +8,13 @@
     </div>
     <div class="col-12 mb-3">
 
-        @isset($article_form->image)
+        {{-- @isset($article_form->image)
             @if ($article_form->image)
-            {{-- @if ($article_form->image && is_string($article_form->image)) --}}
                 @foreach (glob(dirname($article_form->image)."/*") as $item)
                     <img src="{{ asset($item) }}" class="avatar avatar-md rounded" alt="A">
                 @endforeach
             @endif
-        @endisset
+        @endisset --}}
 
 
         <input type="file" id="file" accept="image/*" multiple style="display: none" wire:model="article_form.image">

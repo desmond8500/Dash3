c2dc5a21a319e795266773f307682f082c558c33
kratos <desmond8500@gmail.com>
2024-11-03 22:52:25 +0000

    journal search

--- resources/views/livewire/erp/journaux.blade.php
+++ resources/views/livewire/erp/journaux.blade.php
@@ -1,16 +1,10 @@
 <div>
     <div class="d-flex justify-content-between mb-1">
         <h2>Journal d'activité</h2>
-        <div class="input-group">
-            <input type="text" class="form-control" wire:model.defer="search" placeholder="Rechercher" wire:keydown.enter='getArticles'>
-            <button class="btn btn-primary btn-icon" wire:click="getArticles">
-                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-search" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"> <path stroke="none" d="M0 0h24v24H0z" fill="none"></path> <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0"></path> <path d="M21 21l-6 -6"></path> </svg>
-            </button>
-            @if ($search)
-                <button class="btn btn-secondary btn-icon" wire:click="resetSearch">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-x" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"> <path stroke="none" d="M0 0h24v24H0z" fill="none"></path> <path d="M18 6l-12 12"></path> <path d="M6 6l12 12"></path> </svg>
-                </button>
-            @endif
+        <div>
+
+                <input type="text" class="form-control" wire:model.live="search" placeholder="Rechercher" >
+
         </div>
         @component("components.off-canvas", ['button'=>'Todo'])
             <ul>

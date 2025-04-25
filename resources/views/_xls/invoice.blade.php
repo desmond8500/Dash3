059c04abb3700ee245533ae6f5c51d00a2d6c31d
kratos <desmond8500@gmail.com>
2025-04-25 07:07:29 +0000

    export update

--- resources/views/_xls/invoice.blade.php
+++ resources/views/_xls/invoice.blade.php
@@ -4,7 +4,10 @@
             <th>Désignation</th>
             <th>Reference </th>
             <th>Quantite </th>
-            <th>Tarif </th>
+            <th>Prix </th>
+            <th>invoice_section_id </th>
+            <th>priorite_id </th>
+            <th>article_id </th>
         </tr>
     </thead>
     <tbody>
@@ -13,7 +16,10 @@
                 <td>{{ $row->designation }}</td>
                 <td>{{ $row->reference }}</td>
                 <td>{{ $row->quantite }}</td>
-                <td>{{ $row->tarif }}</td>
+                <td>{{ $row->prix }}</td>
+                <td>{{ $row->invoice_section_id }}</td>
+                <td>{{ $row->priorite_id }}</td>
+                <td>{{ $row->article_id ?? 0 }}</td>
             </tr>
         @endforeach
     </tbody>

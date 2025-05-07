cb0f940a9b58bd1c06234995659da0f7867a1288
kratos <desmond8500@gmail.com>
2025-05-07 06:36:03 +0000

    navbar update

--- resources/views/livewire/layout/navbar.blade.php
+++ resources/views/livewire/layout/navbar.blade.php
@@ -1,5 +1,5 @@
 
-<header class="navbar navbar-expand-md d-print-none">
+<header class="navbar navbar-expand-md d-print-none sticky-top ">
     <div class="container-xl">
         <!-- BEGIN NAVBAR TOGGLER -->
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-menu" aria-controls="navbar-menu" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span>

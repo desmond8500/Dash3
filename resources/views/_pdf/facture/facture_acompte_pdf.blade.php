babbdfaa974aa726a9772117debf5fbcde0f72b7
Desmond8500 <desmond8500@gmail.com>
2025-06-30 16:05:42 +0000

    fix

--- resources/views/_pdf/facture/facture_acompte_pdf.blade.php
+++ resources/views/_pdf/facture/facture_acompte_pdf.blade.php
@@ -50,9 +50,9 @@
         @endphp
 
         @if ($devis->tax == 'tva')
-            <td class="text-end" colspan="3">{{ number_format($(total_marge*1.18) - $total_acompte, 0,'.', ' ') }} F CFA</td>
+            <td class="text-end" colspan="3">{{ number_format(($total_marge*1.18) - $total_acompte, 0,'.', ' ') }} F CFA</td>
         @elseif($devis->tax == 'brs')
-            <td class="text-end" colspan="3">{{ number_format($(total_marge*1.05) - $total_acompte, 0,'.', ' ') }} F CFA</td>
+            <td class="text-end" colspan="3">{{ number_format(($total_marge*1.05) - $total_acompte, 0,'.', ' ') }} F CFA</td>
         @else
             <td class="text-end" colspan="3">{{ number_format($total_marge - $total_acompte, 0,'.', ' ') }} F CFA</td>
         @endif

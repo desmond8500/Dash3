deb13a32b03123f46480abcc17489d965c390e09
kratos <desmond8500@gmail.com>
2025-08-15 02:41:31 +0000

    fix

--- resources/views/_form/webpage_form.blade.php
+++ resources/views/_form/webpage_form.blade.php
@@ -8,7 +8,7 @@
         @else
             <img src="{{ $webpage_form->logo->temporaryUrl() }}" alt="" class="avatar avatar-xl rounded avatar-upload">
         @endif
-        <label for="file" href="#" class="avatar avatar-upload rounded">
+        <label for="file" href="#" class="avatar avatar-xl avatar-upload rounded">
             <i class="ti ti-edit text-muted"></i>
             <span class="avatar-upload-text">Modifier</span>
         </label>
@@ -21,7 +21,7 @@
     <input type="file" id="file" accept="image/*" style="display: none" wire:model="webpage_form.logo">
 </div>
 
-<div class="col-md-4 mb-3">
+<div class="col-md-6 mb-3">
     <label class="form-label">Catégories</label>
     <select class="form-select" wire:model="webpage_form.webpage_category_id">
         <option value="">- Select-</option>

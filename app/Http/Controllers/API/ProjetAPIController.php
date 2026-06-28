55ca592c3c20de6b93e7ebefdab8cef20cec8e29
Desmond8500 <desmond8500@gmail.com>
2026-06-28 23:00:02 +0000

    Ajout des endpoints API pour la gestion des projets : création, mise à jour et suppression, avec documentation OpenAPI mise à jour.

--- app/Http/Controllers/API/ProjetAPIController.php
+++ app/Http/Controllers/API/ProjetAPIController.php
@@ -40,11 +40,42 @@ class ProjetAPIController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
+     *@OA\Post(
+     *      path="/api/v1/projets",
+     *      tags={"Projets",},
+     *      summary="Ajouter un projet",
+     *      @OA\RequestBody(
+     *          required=true,
+     *          @OA\JsonContent(
+     *              required={"name","client_id"},
+     *              @OA\Property(property="client_id", type="integer", example="1"),
+     *              @OA\Property(property="name", type="string", example="Projet Name"),
+     *              @OA\Property(property="description", type="string", example=""),
+     *              @OA\Property(property="favorite", type="string", example="1"),
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=201,
+     *          description="Projet créé avec succès",
+     *       ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="Erreur lors de la création du projet",
+     *       ),
+     *     )
      */
     public function store(Request $request)
     {
-        //
+        $projet = new Projet();
+        $projet->client_id = $request->client_id;
+        $projet->name = $request->name;
+        $projet->description = $request->description;
+
+        if ($projet->save()) {
+            return ResponseController::response(true, 'Projet créé avec succès', $projet, 201);
+        } else {
+            return ResponseController::response(false, 'Erreur lors de la création du projet', null, 400);
+        }
     }
 
     /**
@@ -80,19 +111,100 @@ class ProjetAPIController extends Controller
     }
 
     /**
-     * Update the specified resource in storage.
+     *@OA\Put(
+     *      path="/api/v1/projets/{id}",
+     *      tags={"Projets",},
+     *      summary="Mettre à jour un projet",
+     *      @OA\Parameter(
+     *          name="id",
+     *          in="path",
+     *          required=true,
+     *          description="ID du projet",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *      @OA\RequestBody(
+     *          required=true,
+     *              @OA\JsonContent(
+     *              required={"name","client_id"},
+     *              @OA\Property(property="client_id", type="integer", example="1"),
+     *              @OA\Property(property="name", type="string", example="Projet Name"),
+     *              @OA\Property(property="description", type="string", example=""),
+     *              @OA\Property(property="favorite", type="string", example="1"),
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Client mis à jour avec succès",
+     *       ),
+     *      @OA\Response(
+     *          response=404,
+     *          description="Client non trouvé",
+     *       ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="Erreur lors de la mise à jour du client",
+     *       ),
+     *     )
      */
     public function update(Request $request, string $id)
     {
-        //
+        $projet = Projet::find($id);
+        if($projet){
+            $projet->client_id = $request->client_id;
+            $projet->name = $request->name;
+            $projet->description = $request->description;
+            if ($projet->save()) {
+                return ResponseController::response(true, 'Projet mis à jour avec succès', $projet, 200);
+            } else {
+                return ResponseController::response(false, 'Erreur lors de la mise à jour du projet', null, 400);
+            }
+        } else {
+            return ResponseController::response(false, 'Projet non trouvé', null, 404);
+        }
     }
 
     /**
-     * Remove the specified resource from storage.
+     *@OA\Delete(
+     *      path="/api/v1/projets/{id}",
+     *      tags={"Projets",},
+     *      summary="Supprimer un projet",
+     *      @OA\Parameter(
+     *          name="id",
+     *          in="path",
+     *          required=true,
+     *          description="ID du projet",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Projet supprimé avec succès",
+     *       ),
+     *      @OA\Response(
+     *          response=404,
+     *          description="Projet non trouvé",
+     *       ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="Erreur lors de la suppression du projet",
+     *       ),
+     *     )
      */
     public function destroy(string $id)
     {
-        //
+        $projet = Projet::find($id);
+        if ($projet) {
+            if ($projet->delete()) {
+                return ResponseController::response(true, 'Projet supprimé avec succès', null, 200);
+            } else {
+                return ResponseController::response(false, 'Erreur lors de la suppression du Projet', null, 400);
+            }
+        } else {
+            return ResponseController::response(false, 'Projet non trouvé', null, 404);
+        }
     }
 
     /**
